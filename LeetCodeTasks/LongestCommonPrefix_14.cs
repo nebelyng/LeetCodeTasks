3d67a4b0b35897c12b02f8c5857113331e9de6b1
Alexander <firelord60@gmail.com>
2021-06-28 14:12:07 +0300

    Initial commit

---
+++ LeetCodeTasks/LongestCommonPrefix_14.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodeTasks
+{
+    public class LeetCodeTasks
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+
+            string prefix = "";
+            int minLenth = strs.Min(x => x.Length);
+
+            if (minLenth == 0)
+            {
+                return prefix;
+            }
+
+            for (int j = 0; j < minLenth; j++)
+            {
+                prefix = String.Concat(prefix, strs[0][j]);
+
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    if (strs[i][j] != prefix[j])
+                    {
+                        return prefix.Remove(prefix.Length - 1);
+                    }
+                }
+            }
+
+            return prefix;
+        }
+    }
+}
---
+++ LeetCodeTasks/NumberofGoodPairs_1512.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeTasks
+{
+    class NumberofGoodPairs_1512
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int countPairs = 0;
+            Dictionary<int, int> countNum = new Dictionary<int, int>();
+            foreach (int i in nums)
+            {
+                if (countNum.ContainsKey(i))
+                    countNum[i]++;
+                else
+                    countNum[i] = 1;
+            }
+
+            foreach(int val in countNum.Values)
+            {
+                countPairs += val * (val - 1) / 2;
+            }
+            return countPairs;
+        }
+    }
+}
