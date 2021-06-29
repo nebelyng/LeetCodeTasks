5fe3371267af71d70c4aa5bc6db48d6e44f081d6
Alexander <firelord60@gmail.com>
2021-06-29 13:57:19 +0300

    Create ParkingSystem_1603.cs

---
+++ LeetCodeTasks/ParkingSystem_1603.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCodeTasks
+{
+    class ParkingSystem
+    {
+        int[] slot = new int[3];
+        public ParkingSystem(int big, int medium, int small)
+        {
+            slot[0] = big;
+            slot[1] = medium;
+            slot[2] = small;
+        }
+
+        public bool AddCar(int carType)
+        {
+            --carType;
+            if(slot[carType] > 0)
+            {
+                --slot[carType];
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
