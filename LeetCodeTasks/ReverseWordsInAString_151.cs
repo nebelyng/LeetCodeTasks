3efc278ca2c41d64459f55b84ac9248e53774900
Nebelyng <firelord60@gmail.com>
2021-07-14 16:56:29 +0300

    Create ReverseWordsInAString_151.cs

---
+++ LeetCodeTasks/ReverseWordsInAString_151.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodeTasks
+{
+    class ReverseWordsInAString_151
+    {
+        public string ReverseWords(string s)
+        {
+            return string.Join(' ', s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Reverse());
+        }
+    }
+}
