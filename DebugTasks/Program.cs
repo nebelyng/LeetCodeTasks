7313ca9915a3dbffc8d3c53bbf5aef0c6a6850c0
Nebelyng <firelord60@gmail.com>
2021-07-19 07:26:00 +0300

    Model

---
+++ DebugTasks/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using LeetCodeTasks;
+using System.Linq;
+
+namespace DebugTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            User[] users = Init();
+
+            var sortedUser = users.OrderByDescending(u => u.Posts.Count());
+            var preLast = sortedUser.Skip(1).First();
+            double avarageAge = users.Average(u => u.Age);
+            var olderThen21 = users.Where(u => u.Age > 21);
+            var haventPosts = users.Where(u => u.Posts == null);
+
+            //найти у кого больше постов среди юзеров 2
+            //второй снизу по количесту постов 1 done
+            //средний возраст юзера 1 done
+            //найти всех юзеров которым больше чем 21 1 done
+            //найти всех юзеров у которых нет постов 2 done
+            //найти юзера у которого в коментариях слово блять 3
+            //найти юзереров и все коментарии юзеров у которых есть слово блять 4
+
+            static void AddPost(string header, string textPost, User user)
+            {
+                Post newPost = new Post() { Header = header, TextPost = textPost, User = user };
+                user.Posts.Add(newPost);
+            }
+
+            static void AddComm(User user, string textComment, Post comentedPost)
+            {
+                Comment newComm = new Comment() { User = user, TextComment = textComment };
+                comentedPost.Coments.Add(newComm);
+            }
+
+            static User[] Init()
+            {
+                User user = new User();
+                User user2 = new User();
+
+                user.Name = "vasya";
+                user2.Name = "kolya";
+
+                user.Age = 25;
+                user2.Age = 18;
+
+                AddPost("post", "lalala", user);
+                AddPost("post2", "lalala", user);
+                AddPost("post3", "lalala", user);
+
+                AddPost("post", "lalala", user2);
+                AddPost("post2", "lalala", user2);
+                AddPost("post3", "lalala", user2);
+                AddPost("post4", "lalala", user2);
+                AddPost("post5", "lalala", user2);
+
+                AddComm(user, "hahaha1", user.Posts[1]);
+                AddComm(user2, "hahaha2", user.Posts[1]);
+                AddComm(user, "hahaha3", user.Posts[1]);
+
+                AddComm(user, "hahaha1", user2.Posts[0]);
+                AddComm(user2, "hahaha2", user2.Posts[0]);
+                AddComm(user, "hahaha3", user2.Posts[0]);
+
+                return new[] { user, user2 };
+            }
+
+        }
+    }
+}
---
+++ LeetCodeTasks/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public string Header { get; set; }
+        public string TextPost { get; set; }
+        public User User { get; set; }
+        public List<Comment> Coments { get; set; }
+    }
+
+    public class Comment
+    {
+        public User User { get; set; }
+        public string TextComment { get; set; }
+    }
+}
